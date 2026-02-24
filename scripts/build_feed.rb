799a6895cb0eea73ba7c3fec7bfd3516bc4ecede
shigeru.nakajima <shigeru.nakajima@gmail.com>
2026-02-25 00:31:15 +0900

    Fix FusionRss initializer keyword in build script

--- scripts/build_feed.rb
+++ scripts/build_feed.rb
@@ -125,7 +125,7 @@ def main
   log "Loaded #{feeds.length} feeds"
   log "Loaded #{blacklist_rules.length} blacklist rules"
 
-  merged_items = FusionRss.new(blacklist_rules: blacklist_rules, max_items: MAX_ITEMS)
+  merged_items = FusionRss.new(blacklist_rules: blacklist_rules, max_feeds: MAX_ITEMS)
 
   feeds.each do |feed|
     fetched_at = Time.now
