5580404e868d11c99cc47bce424c9b4d7f9875ac
shigeru.nakajima <shigeru.nakajima@gmail.com>
2026-02-26 21:27:43 +0900

    Use open-uri for feed fetching

--- scripts/subscribe_rss.rb
+++ scripts/subscribe_rss.rb
@@ -1,14 +1,12 @@
 # frozen_string_literal: true
 
-require 'net/http'
-require 'uri'
+require 'open-uri'
 
 # SubscribeRss represents a feed source and provides HTTP fetching.
 class SubscribeRss
   USER_AGENT = 'rss-merge-bot/0.1'
   OPEN_TIMEOUT = 10
   READ_TIMEOUT = 20
-  MAX_REDIRECTS = 3
 
   attr_reader :name, :url
 
@@ -17,36 +15,15 @@ class SubscribeRss
     @url = url
   end
 
-  def http_get(target_url = url, limit: MAX_REDIRECTS)
-    raise "Too many redirects while fetching #{target_url}" if limit.negative?
-
-    uri = URI.parse(target_url)
-    raise "Unsupported URL scheme for #{target_url}" unless %w[http https].include?(uri.scheme)
-
-    request = Net::HTTP::Get.new(uri)
-    request['User-Agent'] = USER_AGENT
-
-    response = Net::HTTP.start(
-      uri.host,
-      uri.port,
-      use_ssl: uri.scheme == 'https',
+  def http_get(target_url = url)
+    # rubocop:disable Security/Open
+    # open-uri is acceptable here because URLs are sourced from feeds.yml only.
+    URI.open(
+      target_url,
+      'User-Agent' => USER_AGENT,
       open_timeout: OPEN_TIMEOUT,
       read_timeout: READ_TIMEOUT
-    ) do |http|
-      http.request(request)
-    end
-
-    case response
-    when Net::HTTPSuccess
-      response.body.to_s
-    when Net::HTTPRedirection
-      location = response['location']
-      raise "Redirect response without Location for #{target_url}" if location.to_s.strip.empty?
-
-      redirected = URI.join(target_url, location).to_s
-      http_get(redirected, limit: limit - 1)
-    else
-      raise "HTTP #{response.code} #{response.message} for #{target_url}"
-    end
+    ).call(&:read)
+    # rubocop:enable Security/Open
   end
 end
