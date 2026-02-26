ff78b67fcef9ce5af3236021fa1cb66669e25d6a
shigeru.nakajima <shigeru.nakajima@gmail.com>
2026-02-26 23:45:56 +0900

    Split SubscribeRss into directory files

---
+++ scripts/subscribe_rss/feed_entry.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require_relative '../fusion_rss/feed_entry'
+
+class SubscribeRss
+  FeedEntry = Data.define(:title, :url, :published_at, :feed_name) do
+    def to_fusion_entry
+      base = title.to_s.strip
+      base = '(no title)' if base.empty?
+      FusionRss::FeedEntry.new(
+        title: "[#{feed_name}] #{base}",
+        url: url,
+        published_at: published_at,
+        feed_name: feed_name
+      )
+    end
+  end
+end
